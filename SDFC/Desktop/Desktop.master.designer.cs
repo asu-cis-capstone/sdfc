c051dcd0f4cd1966f017dd099e5feef5c91e05b9
matthewokamoto <mokamoto@asu.edu>
2015-03-20 06:41:08 -0700

    a

--- SDFC/Desktop/Desktop.master.designer.cs
+++ SDFC/Desktop/Desktop.master.designer.cs
@@ -21,6 +21,15 @@ public partial class Desktop_Desktop {
     protected global::System.Web.UI.WebControls.Literal PageTitle;
     
     /// <summary>
+    /// head control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+    
+    /// <summary>
     /// formSearch control.
     /// </summary>
     /// <remarks>
